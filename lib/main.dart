4d1a33307b14ed06dae92d57d205bb300c0a53e8
Bartosz Kraszewski <bkraszewski@gmail.com>
2019-09-26 21:11:29 +0200

    step #2 getting data from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,51 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:yoapp/person.dart';
+import 'package:yoapp/repository.dart';
 
 void main() => runApp(YoApp());
 
 class YoApp extends StatelessWidget {
+
+  var repository = Repository();
+
   @override
   Widget build(BuildContext context) {
-    var persons = ["Welcome", "To", "Programistok"];
-
     return MaterialApp(home: Scaffold(
       backgroundColor: Colors.green,
-      body: ListView.builder(
-          itemCount: persons.length,
-          itemBuilder: (context, position) {
-            return YoListItem(persons[position]);
-          }),
+      body: StreamBuilder<List<Person>>(
+        stream: repository.getAllPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting :
+              return Text("Loading...");
+            default:
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, position) {
+                    return YoListItem(snapshot.data[position]);
+                  });
+          }
+        },
+      ),
     ));
   }
 }
 
-class YoListItem extends StatelessWidget{
+//ListView.builder(
+//itemCount: persons.length,
+//itemBuilder: (context, position) {
+//return YoListItem(persons[position]);
+//})
 
-  final String name;
+class YoListItem extends StatelessWidget {
 
-  YoListItem(this.name);
+  final Person person;
+
+  YoListItem(this.person);
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +53,7 @@ class YoListItem extends StatelessWidget{
       color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
       child: Padding(
         padding: EdgeInsets.all(16),
-        child: Text(name.toUpperCase(),
+        child: Text(person.name.toUpperCase(),
           style: TextStyle(
               color: Colors.white,
               fontSize: 34,
--- lib/repository.dart
+++ lib/repository.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:yoapp/person.dart';
 
 class Repository {
   Stream<List<Person>> getAllPeople() {
-    // TODO
+    return Firestore.instance.collection("people")
+        .snapshots()
+        .map((querySnapshot){
+          return querySnapshot.documents.map((document){
+            return Person(document['name'], document['token']);
+          }).toList();
+    });
   }
 
   void saveNewPerson(Person person) {
