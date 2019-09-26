10aadcb45442f744b438295ee0703db862187530
Bartosz Kraszewski <bkraszewski@gmail.com>
2019-09-26 20:37:21 +0200

    Step #1 List of dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(YoApp());
@@ -5,6 +7,39 @@ void main() => runApp(YoApp());
 class YoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    var persons = ["Welcome", "To", "Programistok"];
+
+    return MaterialApp(home: Scaffold(
+      backgroundColor: Colors.green,
+      body: ListView.builder(
+          itemCount: persons.length,
+          itemBuilder: (context, position) {
+            return YoListItem(persons[position]);
+          }),
+    ));
   }
+}
+
+class YoListItem extends StatelessWidget{
+
+  final String name;
+
+  YoListItem(this.name);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text(name.toUpperCase(),
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 34,
+              fontWeight: FontWeight.bold
+          ),
+          textAlign: TextAlign.center,),),
+    );
+  }
+
 }
